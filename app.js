429e47d491bbf34a8c301787ae17dff92077ea0d
ganapurba007 <ganapurbakusuma86@gmail.com>
2026-08-24 15:15:27 +0700

    feat: configure database connection

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 const express = require("express");
 const mysql = require("mysql2");
+const { Sequelize, DataTypes } = require("sequelize");
 require("dotenv").config();
 const env = process.env;
 const app = express();
 const port = env.PORT;
 
-const connection = mysql.createConnection({
-  host: env.DB_HOST,
-  user: env.DB_USERNAME,
-  password: env.DB_PASSWORD,
-  database: env.DB_NAME,
+const sequelize = new Sequelize(env.DB_NAME, env.DB_USERNAME, env.DB_PASSWORD, {
+  host: env.DB_HOST || "localhost",
+  dialect: "mysql",
+  logging: false,
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.log("Error connecting to DB", err);
-  } else {
-    console.log("Connected to DB");
-  }
-});
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
