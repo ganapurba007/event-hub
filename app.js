d236bab211be9a6f3b4f6e4e3f88ac722ba2e261
ganapurba007 <ganapurbakusuma86@gmail.com>
2026-09-09 08:58:50 +0700

    fix(vercel): format includeFiles as array and add partials to express views path

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.use(
 
 // set view engine
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", [path.join(__dirname, "views"), path.join(__dirname, "partials")]);
 
 const sequelize = new Sequelize(
   env.DB_NAME || "event-management",
